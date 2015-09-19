956ea9d209aeb81d9d69848918221ba21f087e2d
Gabriele Petronella <gabriele@buildo.io>
2015-09-19 11:41:34 +0100

    Initial Commit

---
+++ NightVigilant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NightVigilant
+//
+//  Created by Gabriele Petronella on 9/19/15.
+//  Copyright © 2015 buildo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
